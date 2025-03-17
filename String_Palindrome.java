f1be32d61abaa9bf183431ea92908592be0d7b0b
Kalash <kalashkose0@gmail.com>
2025-03-17 18:00:42 +0530

    init commit

--- String_Palindrome.java
+++ String_Palindrome.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class String_Palindrome {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter String :- ");
+        String str = sc.nextLine();
 
+        str= str.toLowerCase();
 
+        char charArray[]= str.toCharArray();
+        int start = 0;
+        int end = str.length()-1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (charArray[start] != charArray[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("No, not a palindrome");
+        }
     }
 }
