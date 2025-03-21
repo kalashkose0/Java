ea7b98c867378cd90b128699042a24d195e9492b
Kalash <kalashkose0@gmail.com>
2025-03-21 21:48:37 +0530

    init commit

---
+++ LL_make.java
@@ -0,0 +1,23 @@
+public class LL_make {
+
+    Node head;
+
+    class Node{
+        String data;
+        Node next;
+
+
+        public Node(String data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+
+    public static void main(String []args){
+
+    Linkedlist list = new Linkedlist();
+
+
+    }
+}
