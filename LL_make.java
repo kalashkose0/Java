5fc59ac4f574f5cb75947bfcdb34e44721d6362d
Kalash <kalashkose0@gmail.com>
2025-03-22 21:29:12 +0530

    init commit

--- LL_make.java
+++ LL_make.java
@@ -1,23 +1,46 @@
-public class LL_make {
+class Node {
+    int data;
+    Node next;
 
-    Node head;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
-    class Node{
-        String data;
-        Node next;
+class LinkedList {
+    private Node head;
 
-
-        public Node(String data, Node next) {
-            this.data = data;
-            this.next = next;
-        }
+    public LinkedList() {
+        this.head = null;
     }
 
+    // ----- Method to add a new node at the beginning of the list
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
 
-    public static void main(String []args){
+    // -----Method to display the list
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
 
-    Linkedlist list = new Linkedlist();
+public class LL_make {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
 
+        list.addFirst(10);
+        list.addFirst(20);
+        list.addFirst(30);
 
+        list.display();  // Output should be: 30 20 10
     }
 }
